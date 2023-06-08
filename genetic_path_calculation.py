d2757a562082288ca92ab07d95dfe625d1f49423
VictorMauroy <mauroy.victor@gmail.com>
2023-06-08 14:23:51 +0200

    Feat: :construction: create function names for genetic calculation

---
+++ genetic_path_calculation.py
@@ -0,0 +1,17 @@
+def generate_path_population(population_count:int) -> list[dict]:
+    """Generate (population_count) random paths from origin to a random point 
+    and return a list of dictionary corresponding to those paths
+
+    Args:
+        population_count (int): How many paths to generate
+
+    Returns:
+        list[dict]: a list of dictionary corresponding to the paths created
+    """
+    
+
+def evaluate_path_population() :
+    """"""
+
+def select_best_paths() :
+    """"""
--- main.py
+++ main.py
@@ -2,15 +2,12 @@ import random
 from random import randint
 
 # dict of str : tuple
-planet_positions = {
-    "origin" : (0, 0)
-}
+planet_positions = []
 random.seed(67)
 
 char = 'a'
 for i in range(0,20) :
-    print (char)  
     planet_positions[char] = (randint(-500, 500), randint(-500, 500))
     char = chr(ord(char) + 1)
     
-  
+print(planet_positions)
