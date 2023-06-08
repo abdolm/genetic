615fd8e1364984253753c356b24c496d9aab1dbb
VictorMauroy <mauroy.victor@gmail.com>
2023-06-08 14:40:26 +0200

    Feat: :sparkles: create a class for genetic path computation and add a constructor and a function to generate a population of paths

--- genetic_path_calculation.py
+++ genetic_path_calculation.py
@@ -1,17 +1,29 @@
-def generate_path_population(population_count:int) -> list[dict]:
-    """Generate (population_count) random paths from origin to a random point 
-    and return a list of dictionary corresponding to those paths
+class Genetic_Computation :
+    def __init__(self, points_to_visit:list[tuple]) -> None:
+        self.points_to_visit = points_to_visit
+        self.population = []
 
-    Args:
-        population_count (int): How many paths to generate
+    def generate_path_population(self, population_size:int) -> list[dict]:
+        """Generate (population_count) random paths from origin to a random point 
+        and return a list of dictionary corresponding to those paths
 
-    Returns:
-        list[dict]: a list of dictionary corresponding to the paths created
-    """
-    
+        Args:
+            population_count (int): How many paths to generate
 
-def evaluate_path_population() :
-    """"""
+        Returns:
+            list[dict]: a list of dictionary corresponding to the paths created
+        """
+        from random import shuffle
 
-def select_best_paths() :
-    """"""
+        for _ in range(population_size) :
+            points_index = [i for i in range(1, len(self.points_to_visit))]
+            shuffle(points_index)
+            points_index.insert(0, self.points_to_visit[0])
+
+        print(self.population)
+
+    def evaluate_path_population(self) :
+        """"""
+
+    def select_best_paths(self) :
+        """"""
--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from random import randint
 # dict of str : tuple
 planet_positions = []
 random.seed(67)
+
 for i in range(20) :
     planet_positions.append((randint(-500, 500), randint(-500, 500)))
     
 print(planet_positions)
-  
