b72202c27ba6ebefd9975cafa170d15fbb5a3f28
simplon <simplonetudiant59@gmail.com>
2023-06-08 14:22:18 +0200

    do the lis

--- main.py
+++ main.py
@@ -2,15 +2,10 @@ import random
 from random import randint
 
 # dict of str : tuple
-planet_positions = {
-    "origin" : (0, 0)
-}
+planet_positions = []
 random.seed(67)
-
-char = 'a'
-for i in range(0,20) :
-    print (char)  
-    planet_positions[char] = (randint(-500, 500), randint(-500, 500))
-    char = chr(ord(char) + 1)
+for i in range(20) :
+    planet_positions.append((randint(-500, 500), randint(-500, 500)))
     
+print(planet_positions)    
   
